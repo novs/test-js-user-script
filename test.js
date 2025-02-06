8f00289e4bb0cfd6f01bf47a1bbae4200f10eff1
Georgiy Sosnovskiy <gsosnovskiy@gegi.co>
2025-02-06 14:17:16 +0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Test
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Test
+// @author       Roman Passler
+// @match        https://*
+// @match        http://*
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    console.log('Test!');
+})();
